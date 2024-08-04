75e3c9a2d28ed07a716907e104f498848119e6f8
Victoria Orleanova <Vika9_96@mail.ru>
2024-08-04 23:17:57 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let absNumber = Math.abs(n);
+  let stringNumber = String(absNumber);
+  let arrayNumber = Array.from(stringNumber, Number);
+  let reverseArray = arrayNumber.reverse();
+  let stringReversedArray = reverseArray.join('');
+  let reversedNumber = Number(stringReversedArray);
+  return reversedNumber;
+
+  //long version in one string
+  //return Number(Array.from(String(Math.abs(n)), Number).reverse().join(''))
 }
